903a68edaf2319358a44ba03c6416e212216e3b8
krey35 <kenneth.reyes@kellogg.northwestern.edu>
2022-05-15 07:21:15 +0000

    add Index & Show GET actions in places.controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,12 @@
 class PlacesController < ApplicationController
+    #GET action to read a list of all places (HTTP GET Method) Path /places
+    # reads from index.html.erb
+    def index
+        @places = Place.all 
+    end
+    #SHOW action to read info of a single place (HTTP GET Method) Path /places/abc
+    #reads from show.html.erb
+    def show
+        @place = Place.find_by({ "id" => params["id"] })
+    end 
 end
